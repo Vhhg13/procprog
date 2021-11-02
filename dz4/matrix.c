21be5b43b1c65d1bda35f3ffdd7d4da34c4ea961
Vhhg <max362075@mail.ru>
2021-11-02 13:08:35 +0300

    2-11

--- dz4/matrix.c
+++ dz4/matrix.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+// C_i == Количество продавцов
+#define C_i 3
+#define C_j 2
 
 int main(void){
   float A[3][4]={
@@ -10,22 +13,81 @@ int main(void){
     {2.8, 0.4},
     {5.0, 1.0},
     {2.0, 1.5}};
-  float C[3][2];
+  float C[C_i][C_j];
   float res;
-  for(int i=0;i<3;++i)
-    for(int j=0;j<2;++j){
+  
+  for(int i=0;i<C_i;++i)
+    for(int j=0;j<C_j;++j){
       res=0;
       for(int k=0;k<4;++k)
 	res+=A[i][k]*B[k][j];
       C[i][j]=res;
     }
   
-  for(int i=0;i<3;++i){
-    for(int j=0;j<2;++j)
-      printf("%.1f ", C[i][j]);
-    putchar(10);
-  }
+  /* for(int i=0;i<C_i;++i){ */
+  /*   for(int j=0;j<C_j;++j) */
+  /*     printf("%.1f ", C[i][j]); */
+  /*   putchar(10); */
+  /* } */
+  /* putchar(10); */
 
+
+  
+  // 1
+  float mx=0, mn=1000;
+  int ind_min, ind_max;
+  for(int i=0;i<C_i;++i){
+    if(C[i][0]<mn){
+      mn=C[i][0];
+      ind_min=i;
+    }
+    if(C[i][0]>mx){
+      mx=C[i][0];
+      ind_max=i;
+    }
+  }
+  printf("1) min: %d, max: %d\n", ind_min+1, ind_max+1);
+
+
+  
+  // 2
+  mx=0; mn=1000;
+  for(int i=0;i<C_i;++i){
+    if(C[i][1]<mn){
+      mn=C[i][1];
+      ind_min=i;
+    }
+    if(C[i][1]>mx){
+      mx=C[i][1];
+      ind_max=i;
+    }
+  }
+  printf("2) min: %d, max: %d\n", ind_min+1, ind_max+1);
+
+
+  
+  // 3
+  res=0;
+  for(int i=0;i<C_i;++i)
+    res+=C[i][0];
+  printf("3) %.1f\n", res);
+
+
+  
+  // 4
+  res=0;
+  for(int i=0;i<C_i;++i)
+    res+=C[i][1];
+  printf("4) %.1f\n", res);
+
+
+  
+  // 5
+  res=0;
+  for(int i=0;i<C_i;++i)
+    for(int j=0;j<C_j;++j)
+      res+=C[i][j];
+  printf("5) %.1f\n", res);
   
 
   
